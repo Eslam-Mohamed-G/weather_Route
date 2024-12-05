981878378f782579a9d717870901834e577390a0
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-12-05 17:54:07 +0200

    style for footer

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,9 @@ var menuContent = document.querySelector(".menu");
 menuBTN.addEventListener("click", function(){
     menuContent.classList.toggle("open");
 })
+
+
+// API Key: 787f5ac38c8d46be95d133218240512
+
+// var keyAPI = "787f5ac38c8d46be95d133218240512";
+// var url = "https://api.weatherapi.com/v1/forecast.json";
